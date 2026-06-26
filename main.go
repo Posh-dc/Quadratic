9004769da27a9fde8720b0e246bca7abdfd6d395
aysalami <ayosalami560@gmail.com>
2026-06-26 10:54:11 +0100

    midle point

--- main.go
+++ main.go
@@ -12,14 +12,25 @@ import (
 
 var tpls *template.Template
 
-func quadratic(a, b, c float64) (float64, float64) {
+func quadratic(a, b, c float64) (float64, float64, string) {
+	// Not a quadratic equation
+	if a == 0 {
+		return 0, 0, "a cannot be zero"
+	}
 
-	det := math.Sqrt(((b) * (b)) - (4 * a * c))
+	discriminant := b*b - 4*a*c
 
-	result1 := (-1*b + det) / (2 * a)
-	result2 := (-1*b - det) / (2 * a)
+	// No real roots
+	if discriminant < 0 {
+		return 0, 0, "equation has no real roots"
+	}
 
-	return result1, result2
+	sqrtD := math.Sqrt(discriminant)
+
+	x1 := (-b + sqrtD) / (2 * a)
+	x2 := (-b - sqrtD) / (2 * a)
+
+	return x1, x2, ""
 }
 
 func homehandler(w http.ResponseWriter, r *http.Request) {
@@ -27,6 +38,12 @@ func homehandler(w http.ResponseWriter, r *http.Request) {
 	tpl, _ := template.ParseFiles("index.html")
 
 	tpl.Execute(w, "index.html")
+}
+
+type comp struct {
+	Result1 float64
+	Result2 float64
+	Error   string
 }
 
 func quadHandler(w http.ResponseWriter, r *http.Request) {
@@ -37,14 +54,11 @@ func quadHandler(w http.ResponseWriter, r *http.Request) {
 	b, _ := strconv.Atoi(r.FormValue("b"))
 	c, _ := strconv.Atoi(r.FormValue("c"))
 
-	outPUT := make(map[string]float64)
+	x1, x2, err := quadratic(float64(a), float64(b), float64(c))
 
-	result1, result2 := quadratic(float64(a), float64(b), float64(c))
+	outPut := comp{x1, x2, err}
 
-	outPUT["result1"] = result1
-	outPUT["result2"] = result2
-
-	json.NewEncoder(w).Encode(outPUT)
+	json.NewEncoder(w).Encode(outPut)
 	fmt.Println("i got clicked")
 }
 
--- style/index.js
+++ style/index.js
@@ -17,8 +17,22 @@ function req(e) {
         .then(res => res.json())
         .then((data) => {
 
-            document.querySelector("#result1").value = data.result1
-            document.querySelector("#result2").value = data.result2
+            if (data.Error !== "") {
+                document.querySelector(".error-code").innerText = "Error " + "400";
+                document.querySelector(".error-message").innerText = data.Error;
+                document.querySelector("#prevent1").style.display = "block";
+                document.querySelector("#dialog").style.display = "flex";
+                return
+            }
+
+            document.querySelector("#result1").value = data.Result1
+            document.querySelector("#result2").value = data.Result2
         })
 
 }
+
+
+document.querySelector(".close-button").addEventListener("click", () => {
+    document.querySelector("#dialog").style.display = "none";
+    document.querySelector("#prevent1").style.display = "none";
+})
