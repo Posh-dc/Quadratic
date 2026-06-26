c5d93639366465a7b6688356d71da6886a4a621c
aysalami <ayosalami560@gmail.com>
2026-06-26 08:30:48 +0100

    midle point

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"html/template"
+	"math"
 	"net/http"
 	"strconv"
 	// "os"
@@ -11,8 +12,14 @@ import (
 
 var tpls *template.Template
 
-func quadratic(a, b, c int) (int, int) {
-return a+b , b+c
+func quadratic(a, b, c float64) (float64, float64) {
+
+	det := math.Sqrt(((b) * (b)) - (4 * a * c))
+
+	result1 := (-1*b + det) / (2 * a)
+	result2 := (-1*b - det) / (2 * a)
+
+	return result1, result2
 }
 
 func homehandler(w http.ResponseWriter, r *http.Request) {
@@ -30,13 +37,9 @@ func quadHandler(w http.ResponseWriter, r *http.Request) {
 	b, _ := strconv.Atoi(r.FormValue("b"))
 	c, _ := strconv.Atoi(r.FormValue("c"))
 
-	outPUT := make(map[string]int)
+	outPUT := make(map[string]float64)
 
-	
-	
-	
-result1 , result2 := quadratic(a,b,c)
-
+	result1, result2 := quadratic(float64(a), float64(b), float64(c))
 
 	outPUT["result1"] = result1
 	outPUT["result2"] = result2
